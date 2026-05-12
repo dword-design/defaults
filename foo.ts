c2e66f86f96727f70aea1b2bac747a04d580f88c
Sebastian Landwehr <info@sebastianlandwehr.com>
2026-05-12 15:44:10 +0200

    fix: minimal reproductions

--- foo.ts
+++ foo.ts
@@ -1,8 +1,18 @@
-type DeepMerge<TValue, TDefault> = [TValue] extends [Record<string, unknown>]
+type DeepMerge<TValue, TDefault> = [TValue] extends [{ a: number }]
   ? TValue
-  : TDefault
+  : TDefault;
 
 function weakConstraint<T extends { a: number }>() {
-  const foo = {} as DeepMerge<T, { b: number }>;
+  const foo = {} as DeepMerge<T, { b: number }>
   console.log(foo.a);
+  return foo;
 }
+
+const blub = weakConstraint<{ a: number }>();
+
+function f<T extends number>(x: T) {
+    var y: T extends number ? number : string;
+
+    // `T` is not assignable to `T extends number ? number : string`
+    y = x;
+ }
